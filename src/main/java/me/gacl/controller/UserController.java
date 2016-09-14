5d0045f76a872f5c9ef0db676cb4f8161333806c
johnlhh <649220839@qq.com>
2016-09-14 12:57:50 +0800

    add @RestController

--- src/main/java/me/gacl/controller/UserController.java
+++ src/main/java/me/gacl/controller/UserController.java
@@ -1,7 +1,10 @@
 package me.gacl.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by luohh on 2016/8/22.
  */
+@RestController
 public class UserController {
 }
